84c53e1a6c106913ad099a893dc7a541562e5431
hoanglonglouis <97615485+hoanglonglouis@users.noreply.github.com>
2025-01-20 16:29:24 +0700

    Update AthinStockholm.sh

--- AthinStockholm.sh
+++ AthinStockholm.sh
@@ -34,7 +34,7 @@ for region in "${!region_image_map[@]}"; do
     image_id=${region_image_map[$region]}
 
     # Check if Key Pair exists
-    key_name="GodLong-$region"
+    key_name="MrThin-$region"
     if aws ec2 describe-key-pairs --key-names "$key_name" --region "$region" > /dev/null 2>&1; then
         echo "Key Pair $key_name already exists in $region"
     else
